4f88310a8e5def3989bb4d6731b94d0cd4f5cad8
Matt Dorn <matt.dorn@gmail.com>
2019-12-17 23:58:49 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+```
+git clone https://github.com/mdorn/flask-dance-okta.git
+cd flask-dance-okta
+python3 -m venv env
+. env/bin/activate
+pip install -r requirements.txt
+cp .env.example .env  # modify your .env file with values for your Okta environment
+flask run
+```
---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask, redirect, url_for
+from flask_dance.consumer import OAuth2ConsumerBlueprint
+
+app = Flask(__name__)
+app.secret_key = os.environ.get('FLASK_SECRET_KEY', '$3cr3t')
+
+base_url = os.environ.get('OKTA_BASE_URL')
+okta_blueprint = OAuth2ConsumerBlueprint(
+    'okta', __name__,
+    client_id=os.environ.get('OKTA_CLIENT_ID'),
+    client_secret=os.environ.get('OKTA_CLIENT_SECRET'),
+    base_url=os.environ.get('OKTA_BASE_URL'),
+    token_url='{}/token'.format(base_url),
+    authorization_url='{}/authorize'.format(base_url),
+    scope=['openid', 'profile', 'email']
+    # redirect_url='http://localhost:5000/other',
+)
+
+app.register_blueprint(okta_blueprint, url_prefix='/login')
+
+@app.route('/')
+def index():
+    if not okta_blueprint.session.token:
+        return redirect(url_for('okta.login'))
+    resp = okta_blueprint.session.get('{}/userinfo'.format(base_url))
+    return 'You are {email} on Okta'.format(email=resp.json()['email'])
